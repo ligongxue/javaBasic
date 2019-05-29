1972d810f8844fcb24d40472371e4a3ca3e24a83
ligongxue <2820888548@qq.com>
2019-05-29 19:45:53 +0800

    gg

--- src/com/neuedu/test3/Work.java
+++ src/com/neuedu/test3/Work.java
@@ -1,5 +1,47 @@
 package com.neuedu.test3;
 
 public class Work {
-    
+    /*假设要为某个公司编写雇员工资支付程序。
+    一般工人（Worker）按每月工作的天数计算工资；
+    销售人员（Salesman）在基本工资基础上每月还有销售提成；
+    经理（Manager）每月按固定工资支付，
+    临时工（Floater）按小时支付。
+    要求描述所有雇员的共同特性（如姓名，性别，出生日期，员工类别），
+    含有计算工资的方法computeSalay（）， 所有员工用该方法计薪。*/
+    private String name;
+    private String sex;
+    private String date;
+    private int num;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
